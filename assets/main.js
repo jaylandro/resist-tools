7b804871bab3c27ec456d925d469e24f895afaad
Jay Landro <jaylandro@gmail.com>
2021-04-22 12:55:40 -0700

    Seperate out CSS & JS, add SVGs

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+function shareLocation(mapLink) {
+  if (navigator.share) {
+    navigator
+      .share({
+        title: mapLink.textContent,
+        url: mapLink.href,
+      })
+      .then(() => {
+        console.log("Thanks for sharing!");
+      })
+      .catch(console.error);
+  }
+}
+
+function geoFindMe() {
+  const status = document.querySelector("#status");
+  const mapLink = document.querySelector("#map-link");
+
+  mapLink.href = "";
+  mapLink.textContent = "";
+
+  function success(position) {
+    console.log("Success", position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = "";
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+
+    shareLocation(mapLink);
+  }
+
+  function error() {
+    status.textContent = "Unable to retrieve your location";
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+document.querySelector("#location-share").addEventListener("click", geoFindMe);
